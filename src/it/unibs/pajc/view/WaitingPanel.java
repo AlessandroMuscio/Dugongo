fbf55cfc07dce13603f6bb713c39bd7459c2dba4
AlessandroMuscio <muscioalex30@gmail.com>
2022-09-07 21:52:08 +0200

    WaitingPanel migliorato

--- src/it/unibs/pajc/view/WaitingPanel.java
+++ src/it/unibs/pajc/view/WaitingPanel.java
@@ -6,13 +6,44 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WaitingPanel extends JPanel {
-  
-  private MyLabel lblTitolo;
-  
-  public WaitingPanel(){
-    lblTitolo = new MyLabel("WAITING FOR THE HOST...", SwingConstants.CENTER, 15);
-    
+
+  private MyLabel label;
+
+  public WaitingPanel() {
+    label = new MyLabel("In attesa dell'Host...", SwingConstants.CENTER, 10);
+
     this.setLayout(new BorderLayout());
-    this.add(lblTitolo, BorderLayout.CENTER);
+    this.add(label, BorderLayout.CENTER);
+    new Thread(this::waitingAnimation).start();
+  }
+
+  private void waitingAnimation() {
+    String text = label.getText().replace(".", "");
+    int waitingTime = 500;
+
+    while (true) {
+      for (int i = 0; i < 3; i++) {
+        refreshLabel(text.concat(".".repeat(i + 1)));
+        wait(waitingTime);
+      }
+
+      refreshLabel(text.concat("."));
+      wait(waitingTime);
+    }
+  }
+
+  private void refreshLabel(String text) {
+    label.setText(text);
+
+    label.repaint();
+    label.revalidate();
+  }
+
+  private void wait(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      System.out.println("ERRORE: " + e);
+    }
   }
 }
